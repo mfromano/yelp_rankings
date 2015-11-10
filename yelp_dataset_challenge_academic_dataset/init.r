0bebf54cac699609ce1b5dcffadd7feff41ed10e
Michael Romano <michaelfromano@gmail.com>
2015-11-10 15:26:32 -0500

    updated init.r

--- yelp_dataset_challenge_academic_dataset/init.r
+++ yelp_dataset_challenge_academic_dataset/init.r
@@ -17,19 +17,21 @@ init.graph <- function(file='review_edges.csv')
     return(gr)
 }
 
-downsample.graph <- function()
+downsample.graph <- function(threshold=10)
 {
-    load('review_edges_ds.RData')
+    gr <- init.graph()
+    gr <- graph.threshold(graph=gr, threshold=threshold)
+    save(gr, file=paste('review_edges_ds', toString(threshold), '.RData', sep=''))
 }
 
-split.graphs <- function()
+split.graphs <- function(threshold=10)
 {
-    load('review_edges_ds.RData')
+    load(file=paste('review_edges_ds', toString(threshold), '.RData', sep=''))
     rand.list <- sample(c(1:length(E(gr))), floor(length(E(gr))*.9), replace=FALSE)
     train.list <- E(gr)[rand.list]
     probe.list <- setdiff(E(gr),train.list)
     train.graph <- subgraph.edges(graph=gr, eids=train.list)
     probe.graph <- subgraph.edges(graph=gr, eids=probe.list)
-    save(train.graph, file='review_edges_train.RData')
-    save(probe.graph, file='review_edges_probe.RData')
+    save(train.graph, file=paste('review_edges_train',toString(threshold),'.RData', sep=''))
+    save(probe.graph, file=paste('review_edges_probe',toString(threshold),'.RData', sep=''))
 }
